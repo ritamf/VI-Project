7258001f6207142a077c1f93ea65370b8be50dd7
IvoFelix <ivo.felix@sapo.pt>
2022-01-10 13:06:51 +0000

    Further refinements and a time series for each year

--- line-plot.js
+++ line-plot.js
@@ -17,7 +17,6 @@ $(function () {
 var selectedCountry = "China"; // getSelectedCountry(dropdown);
 var selectedContinent = "Choose Continent";// getSelectedContinent(dropdown);
 var selectedIndicator = "cases"; // getSelectedIndicator(dropdown);
-var selectedYear = 2020;
 
 function preProcessData(data) {
 
@@ -87,41 +86,59 @@ function draw(data) {
         .style("text-decoration", "underline")
         .text("Number of " + selectedIndicator + " in " + selectedCountry);
 
-    data = data.filter(d => d.country == selectedCountry && d.indicator == selectedIndicator); // TODO: update plot after filtering info
+    // TODO: update plot after filtering info
+    data2020 = data.filter(d => d.country == selectedCountry && d.indicator == selectedIndicator && d.year_week.getFullYear() == 2020);
+    data2021 = data.filter(d => d.country == selectedCountry && d.indicator == selectedIndicator && d.year_week.getFullYear() == 2021);
 
     var x_linear_scale = d3.scaleLinear()
         .range([margin, width - margin])
-        .domain([0, data.length - 1])
+        .domain([0, data2020.length - 1])
         .clamp(true);
 
-    var x_extent = d3.extent(data, d => d.year_week);
-    var x_time_scale = d3.scaleTime()
+    var x_extent = data2020.map((d, i) => i);
+    var x_scale = d3.scalePoint()
         .range([margin, width - margin])
-        .domain([x_extent[0], x_extent[1]]);
+        .domain(x_extent);
 
-    // returns a two-size array with min and max values of y from data
-    var y_extent = d3.extent(data, d => d.weekly_count);
+    let y_max = d3.max([d3.max(data2020, d => d.weekly_count),
+                        d3.max(data2021, d => d.weekly_count)]);
     var y_scale = d3.scaleLinear()
         .range([height - margin, margin])
-        .domain([0, y_extent[1]])
+        .domain([0, y_max])
         .nice();
 
-    var circles = svg.selectAll("circle")
-        .data(data)
+    svg.selectAll("circle.series2020")
+        .data(data2020)
         .join("circle")
-        .attr("cx", d => x_time_scale(d.year_week))
+        .attr("cx", (d, i) => x_scale(i))
         .attr("cy", d => y_scale(d.weekly_count))
-        .attr("r", 3);
+        .attr("r", 3)
+        .attr("class", "series2020");
 
-    var x_axis = d3.axisBottom(x_time_scale)
-        .ticks(d3.timeMonth.every(1))
-        .tickFormat(date => {
-            if (d3.timeYear(date) < date) {
-            return d3.timeFormat('%b')(date);
+    svg.selectAll("circle.series2021")
+        .data(data2021)
+        .join("circle")
+        .attr("cx", (d, i) => x_scale(i))
+        .attr("cy", d => y_scale(d.weekly_count))
+        .attr("r", 3)
+        .attr("class", "series2021");
+
+    var x_axis = d3.axisBottom(x_scale);
+
+    let tick_labels = [""];
+    for (let i = 1; i < data2020.length; i++) {
+        if (data2020[i - 1].year_week.getMonth() == data2020[i].year_week.getMonth()) {
+            tick_labels.push("");
+        } else {
+            if (data2020[i - 1].year_week.getFullYear() == data2020[i].year_week.getFullYear()) {
+                tick_labels.push(d3.timeFormat('%b')(data2020[i].year_week));
             } else {
-            return d3.timeFormat('%Y')(date);
+                tick_labels.push(d3.timeFormat('%Y')(data2020[i].year_week));
             }
-        });
+        }
+    }
+
+    x_axis.tickFormat((date, i) => tick_labels[i]);
 
     svg.append("g")
         .attr("class", "x axis")
@@ -134,47 +151,79 @@ function draw(data) {
         .attr("transform", "translate(" + (margin) + ", 0" + ")")
         .call(y_axis);
 
-    var line = d3.line()
-        .x(d => x_time_scale(d.year_week))
+    var line2020 = d3.line()
+        .x((d, i) => x_scale(i))
         .y(d => y_scale(d.weekly_count));
 
     svg.append("path")
-        .attr("d", line(data))
-        .attr("class", "line");
+        .attr("d", line2020(data2020))
+        .attr("class", "line series2020");
+
+    var line2021 = d3.line()
+        .x((d, i) => x_scale(i))
+        .y(d => y_scale(d.weekly_count));
+
+    svg.append("path")
+        .attr("d", line2021(data2021))
+        .attr("class", "line series2021");
+
+    let datapointlabel_width = 130,
+        datapointlabel_height = 55,
+        datapointlabel_margin = 4,
+        datapointlabel_x_origin = x_linear_scale(0)
+        datapointlabel_y_origin = y_scale(0) - (height - margin),
+        datapoint_label_offset = 4,
+        vertical_line_path = d3.path(),
+        triangle_path = d3.path(),
+        triangle_size = 2;
 
     let datapoint_label = svg.append("g")
         .attr("class", "datapointlabel")
         .attr("visibility", "hidden");
 
-    let datapointlabel_width = 130,
-        datapointlabel_height = 50,
-        datapointlabel_margin = 4,
-        datapointlabel_x_origin = x_linear_scale(0) + 2
-        datapointlabel_y_origin = y_scale(0) - (height - margin),
-        triangle_path = d3.path(),
-        triangle_size = 2;
+    svg.append("circle")
+        .attr("class", "datapointlabel series2020")
+        .attr("cx", datapointlabel_x_origin)
+        .attr("cy", datapointlabel_y_origin)
+        .attr("r", 3)
+        .attr("style", "opacity:0.7;fill:black;");
+    
+    svg.append("circle")
+        .attr("class", "datapointlabel series2021")
+        .attr("cx", datapointlabel_x_origin)
+        .attr("cy", datapointlabel_y_origin)
+        .attr("r", 3)
+        .attr("style", "opacity:0.7;fill:black;");
+    
+    vertical_line_path.moveTo(datapointlabel_x_origin, y_scale.range()[0]);
+    vertical_line_path.lineTo(datapointlabel_x_origin, y_scale.range()[1]);
+    vertical_line = svg.append("path")
+        .attr("d", vertical_line_path.toString())
+        .attr("style", "opacity:0.4;")
+        .attr("visibility", "hidden");
 
-    triangle_path.moveTo(datapointlabel_x_origin, datapointlabel_y_origin);
+    triangle_path.moveTo(datapointlabel_x_origin, datapointlabel_y_origin - datapoint_label_offset);
+    triangle_path.lineTo(datapointlabel_x_origin - triangle_size,
+                         datapointlabel_y_origin - triangle_size - datapoint_label_offset);
     triangle_path.lineTo(datapointlabel_x_origin + triangle_size,
-                            datapointlabel_y_origin + triangle_size);
-    triangle_path.lineTo(datapointlabel_x_origin + triangle_size,
-                            datapointlabel_y_origin - triangle_size);
+                         datapointlabel_y_origin - triangle_size - datapoint_label_offset);
     triangle_path.closePath();
     datapoint_label.append("path")
-        .attr("d", triangle_path.toString());
+        .attr("d", triangle_path.toString())
+        .attr("fill", "black");
 
     datapoint_label.append("rect")
             .attr("width", datapointlabel_width)
             .attr("height", datapointlabel_height)
-            .attr("x", datapointlabel_x_origin + triangle_size)
-            .attr("y", datapointlabel_y_origin - datapointlabel_height / 2)
+            .attr("x", datapointlabel_x_origin - datapointlabel_width / 2)
+            .attr("y", datapointlabel_y_origin - datapointlabel_height - datapoint_label_offset - triangle_size)
             .attr("rx", 2)
             .attr("fill", "white")
             .attr("stroke", "black");
     
     let datapointlabel_text = datapoint_label.append("text")
-        .attr("x", datapointlabel_x_origin + triangle_size + datapointlabel_margin)
-        .attr("y", datapointlabel_y_origin - datapointlabel_height / 2 + datapointlabel_margin)
+        .attr("x", datapointlabel_x_origin + datapointlabel_margin - datapointlabel_width / 2)
+        .attr("y", datapointlabel_y_origin + datapointlabel_margin - datapoint_label_offset - triangle_size - datapointlabel_height)
         .attr("class", "datapointlabel")
         .attr("style", "text-anchor:left;dominant-baseline:hanging;")
         
@@ -185,31 +234,57 @@ function draw(data) {
         .attr("class", "week")
         .attr("style", "font-weight: normal;");
     datapointlabel_text.append("tspan")
-        .text("Count: ")
+        .text("2020: ")
         .attr("dy", "1.4em")
-        .attr("x", datapointlabel_x_origin + triangle_size + datapointlabel_margin)
+        .attr("x", datapointlabel_x_origin + datapointlabel_margin - datapointlabel_width / 2 - datapoint_label_offset + datapointlabel_margin)
         .attr("style", "font-weight: bold;");
     datapointlabel_text.append("tspan")
-        .attr("class", "count")
+        .attr("class", "count2020")
+        .attr("style", "font-weight: normal;");
+    datapointlabel_text.append("tspan")
+        .text("2021: ")
+        .attr("dy", "1.4em")
+        .attr("x", datapointlabel_x_origin + datapointlabel_margin - datapointlabel_width / 2 - datapoint_label_offset + datapointlabel_margin)
+        .attr("style", "font-weight: bold;");
+    datapointlabel_text.append("tspan")
+        .attr("class", "count2021")
         .attr("style", "font-weight: normal;");
     
+    let legend_width = 130,
+        legend_height = 55,
+        legend_margin = 4,
+        legend_x_position = width - margin;
+        legend_y_position = height / 2;
+    
+    let legend = svg.append("g")
+        .attr("class", "legend");
+    legend.append("rect")
+        .attr("width", legend_width)
+        .attr("height", legend_height)
+        .attr("x", legend_x_position)
+        .attr("y", legend_y_position - legend_height / 2)
+        .attr("fill", "white")
+        .attr("stroke", "black");
+
     function handleMouseOverEvent(e) {
 
         let mouse = d3.pointer(e),
             data_index = Math.round(x_linear_scale.invert(mouse[0])), 
             draw_area_width = width - margin * 2,
-            increment_size = draw_area_width / (data.length - 1),
+            increment_size = draw_area_width / (data2020.length - 1),
             relative_x = mouse[0] - margin,
             relative_y = mouse[1] - margin,
             x = data_index * increment_size,
-            y = y_scale(data[data_index].weekly_count),
+            y2020 = y_scale(data2020[data_index].weekly_count),
+            y2021 = y_scale(data2021[data_index].weekly_count),
+            min_y = d3.min([y2020, y2021]),
             date_string;
     
         if (relative_x >= 0 && relative_x <= width - margin * 2 &&
             relative_y >= 0 && relative_y <= height - margin * 2 ) {
             
             // create week string
-            startDate = data[data_index].year_week;
+            startDate = data2020[data_index].year_week;
             endDate = new Date(startDate.getFullYear(), 
                                startDate.getMonth(),
                                startDate.getDate() + 6)
@@ -221,17 +296,38 @@ function draw(data) {
             const dateFormat = { month: 'short', day: 'numeric' };
             date_string = String(startDate.getDate()).padStart(2,"0") + "/" + String(startDate.getMonth() + 1).padStart(2,"0") + " - " + String(endDate.getDate()).padStart(2,"0") + "/" + String(endDate.getMonth() + 1).padStart(2,"0");
 
-            // fill datapoint label values and make it visible
+            // fill datapoint label values, make it visible and move it to the right position
             datapoint_label
                 .attr("visibility", "visible")
-                .attr("transform","translate(" + x + "," + y + ")");
+                .attr("transform","translate(" + x + "," + min_y + ")");
             datapoint_label.select("tspan.week")
                 .text(date_string);
-            datapoint_label.select("tspan.count")
-                    .text(data[data_index].weekly_count);
+            datapoint_label.select("tspan.count2020")
+                .text(data2020[data_index].weekly_count);
+            datapoint_label.select("tspan.count2021")
+                .text(data2021[data_index].weekly_count);
+            
+            // repeat for the vertical line and data circles
+            vertical_line_d = vertical_line.attr("d");
+            vertical_line
+                .attr("visibility", "visible")
+                .attr("d", vertical_line_d.slice(0, vertical_line_d.lastIndexOf(",")) + "," + min_y)
+                .attr("transform","translate(" + x + ",0)");
+            svg.select("circle.datapointlabel.series2020")
+                .attr("visibility", "visible")
+                .attr("transform","translate(" + x + "," + y2020 + ")");
+            svg.select("circle.datapointlabel.series2021")
+                .attr("visibility", "visible")
+                .attr("transform","translate(" + x + "," + y2021 + ")");
         } else {
             // hide datapoint label
             svg.select("g.datapointlabel")
+                .attr("visibility", "hidden");
+            
+            // repeat for the vertical line and data circles
+            vertical_line
+                .attr("visibility", "hidden");
+            svg.selectAll("circle.datapointlabel")
                 .attr("visibility", "hidden");
         }
     }
