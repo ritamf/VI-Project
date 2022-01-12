e03a19aeb7224d13b9ee17b7e05dc6448300dd44
ritamf <ritaferrolho@ua.pt>
2022-01-12 20:07:15 +0000

    redo barplot for version 6 of d3js

--- dbp.js
+++ dbp.js
@@ -1,76 +1,64 @@
-// set the dimensions and margins of the graph
-var margin = { top: 30, right: 30, bottom: 70, left: 60 },
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var selectedContinent = "Europe";
 
-// append the svg object to the body of the page
-var svg = d3.select("#div-bar-plot")
-    .append("svg")
+
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 100},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// set the ranges
+var x = d3.scaleBand()
+          .range([0, width])
+          .padding(0.1);
+var y = d3.scaleLinear()
+          .range([height, 0]);
+          
+// append a 'group' element to 'svg-bar-plot'
+// moves the 'group' element to the top left margin
+var svg = d3.select("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
 
-// Initialize the X axis
-var x = d3.scaleBand()
-    .range([0, width])
-    .padding(0.2);
+// get the data
+d3.csv("datasets/cases_deaths/cases_deaths.csv").then(function(data) {
 
-var xAxis = svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
+  // format the data
+  data.forEach(function(d) {
+    d.population = +d.population;
+    d.weekly_count = +d.weekly_count;
+    d.rate_14_day = +d.rate_14_day;
+    d.cumulative_count = +d.cumulative_count;
+  });
+
+  // filter data so that only rows that contain countries within selectedContinent are included
+  data = data.filter(d => d.continent===selectedContinent); // d.continent===selectedContinent; or ["Portugal","Spain"].includes(d.country) for debugging purposes
+
+  // Scale the range of the data in the domains
+  x.domain(data.map(function(d) { return d.country; }));
+  y.domain([0, d3.max(data, function(d) { return d.population; })]);
 
 
-// Initialize the Y axis
-var y = d3.scaleLinear()
-    .range([height, 0]);
-var yAxis = svg.append("g")
-    .attr("class", "myYaxis")
+  // append the rectangles for the bar chart
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.country); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(d.population); })
+      .attr("height", function(d) { return height - y(d.population); })
+      .style("fill", function (d) { return "rgb(0, 0, " +  100 + ")"; });
 
+  // add the x Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-// A function that create / update the plot for a given variable:
-function update(selectedVar, selectedContinent) {
+  // add the y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
-    // Parse the Data
-    d3.csv("datasets/cases_deaths/cases_deaths.csv", function (data) {
-
-        data = data.filter(function (d) { return d.continent == selectedContinent }) 
-
-        data = data.map(function (d) {
-            return {
-                country: d.country,
-                population: d.population,
-                weekly_counts: d.weekly_counts
-            }
-        });
-
-        // X axis
-        x.domain(data.map(function (d) { console.log(data); return d.country; }))
-        xAxis.transition().duration(1000).call(d3.axisBottom(x))
-
-        // Add Y axis
-        y.domain([0, d3.max(data, function (d) { return +d[selectedVar] })]);
-        yAxis.transition().duration(1000).call(d3.axisLeft(y));
-
-        // variable u: map data to existing bars
-        var u = svg.selectAll("rect")
-            .data(data)
-
-        // update bars
-        u
-            .enter()
-            .append("rect")
-            .merge(u)
-            .transition()
-            .duration(1000)
-            .attr("x", function (d) { return x(d.country); })
-            .attr("y", function (d) { return y(d[selectedVar]); })
-            .attr("width", x.bandwidth())
-            .attr("height", function (d) { return height - y(d[selectedVar]); })
-            .attr("fill", "#69b3a2");
-    })
-
-}
-
-// Initialize plot
-update('population', 'Europe')
+});
