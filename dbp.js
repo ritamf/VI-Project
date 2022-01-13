4a8764b18da6b274486d2836f20079658551f5c8
ritamf <ritaferrolho@ua.pt>
2022-01-13 03:19:41 +0000

    add static bar plot

--- dbp.js
+++ dbp.js
@@ -1,33 +1,8 @@
-// ADD DROPDOWNS
-
-$(function () {
-    $("#continentDropdown").load("components/continentsDropdown.htm");
-});
-
-// note: outros dropdowns tao no bar-plot.html
-
-let dropdownWeekNum = document.getElementById('weekNumDropdown');
-dropdownWeekNum.length = 0;
-
-for (let i = 1; i <= 53; i++) {
-    option = document.createElement('option');
-    option.text = weekToString(i);
-    option.value = i;
-    if (i == 20) option.selected = "selected"; // default week number is selected here
-    dropdownWeekNum.add(option);
-}
-
-// var dropdown_continent = "Africa";// document.getElementById("continentDropdown").value;
-// var dropdown_indicator = "cases"; // document.getElementById("indicatorDropdown").value;
-// var dropdown_count = "Raw"; // document.getElementById("country-dropdownCount").value; // other dropdown option: "Raw count"
-// var dropdown_year = 2021; // document.getElementById("yearDropdown").value;
-// var dropdown_week = 4; // document.getElementById("weekNumDropdown").value;
-
-var dropdown_continent = document.getElementById("continentDropdown").value;
-var dropdown_indicator = document.getElementById("indicatorDropdown").value;
-var dropdown_count = document.getElementById("country-dropdownCount").value;
-var dropdown_year = +document.getElementById("yearDropdown").value;
-var dropdown_week = +document.getElementById("weekNumDropdown").value;
+var dropdown_continent = "Europe";
+var dropdown_indicator = "cases";
+var dropdown_count = "Normalized" // other dropdown option: "Raw count"
+var dropdown_year = 2021;
+var dropdown_week = 20;
 
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 20, bottom: 140, left: 100},
@@ -50,81 +25,70 @@ var svg = d3.select("svg")
     .attr("transform", 
           "translate(" + margin.left + "," + margin.top + ")");
 
-updateGraph();
+// get the data
+d3.csv("datasets/cases_deaths/cases_deaths.csv").then(function(data) {
 
-function updateGraph() {
-    // get the data
-    d3.json("datasets/cases_deaths/cases_deaths.json").then(function(data) {
+  preProcessedData = preProcessCovidData(data);
+  preProcessedData = preProcessedData.get(dropdown_indicator).get(dropdown_year).get(dropdown_week);
 
-    preProcessedData = preProcessCovidData(data);
-    preProcessedData = preProcessedData.get(dropdown_indicator).get(dropdown_year).get(dropdown_week);
+  // filter data so that only rows that contain countries within dropdown_continent and that aren't totals are included
+preProcessedData = preProcessedData.filter(d => d.continent===dropdown_continent && d.country.indexOf("(total)") === -1); // d.continent===selectedContinent; or ["Portugal","Spain"].includes(d.country) for debugging purposes
 
-    console.log(
-    dropdown_continent,
-    dropdown_indicator,
-    dropdown_count,
-    dropdown_year,
-    dropdown_week)
+  // Scale the range of the data in the domains
+  x.domain(preProcessedData.map(function(d) { return d.country; }));
+  y.domain([0, d3.max(preProcessedData, function(d) { return dropdown_count == "Raw"? d.weekly_count : d.normalized; })])
+    .nice();
 
-    // filter data so that only rows that contain countries within dropdown_continent and that aren't totals are included
-    preProcessedData = preProcessedData.filter(d => d.continent===dropdown_continent && d.country.indexOf("(total)") === -1); // d.continent===selectedContinent; or ["Portugal","Spain"].includes(d.country) for debugging purposes
-
-    // Scale the range of the data in the domains
-    x.domain(preProcessedData.map(function(d) { return d.country; }));
-    y.domain([0, d3.max(preProcessedData, function(d) { return dropdown_count == "Raw"? d.weekly_count : d.normalized; })])
-        .nice();
-
-        let titleIndicator;
-        if (dropdown_indicator == "cases") {
-            titleIndicator = "cases"
-            if (dropdown_count == "Normalized") {
-                titleIndicator = titleIndicator + " per 100,000 inhabitants";
-            }
-        } else {
-            titleIndicator = "deaths";
-            if (dropdown_count == "Normalized") {
-                titleIndicator = titleIndicator + " per million inhabitants";
-            }
+    let titleIndicator;
+    if (dropdown_indicator == "cases") {
+        titleIndicator = "cases"
+        if (dropdown_count == "Normalized") {
+            titleIndicator = titleIndicator + " per 100,000 inhabitants";
         }
+    } else {
+        titleIndicator = "deaths";
+        if (dropdown_count == "Normalized") {
+            titleIndicator = titleIndicator + " per million inhabitants";
+        }
+    }
 
-        svg.append("text")
-            .attr("x", width / 2)
-            .attr("y", margin * 3 / 4)
-            .attr("text-anchor", "middle")
-            .style("font-size", "16px")
-            .style("text-decoration", "underline")
-            .text("Number of " + titleIndicator + " in " + dropdown_continent.replace("(total continent)",""));
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", margin * 3 / 4)
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "underline")
+        .text("Number of " + titleIndicator + " in " + dropdown_continent.replace("(total continent)",""));
 
-    // append the rectangles for the bar chart
-    svg.selectAll(".bar")
-        .data(preProcessedData)
-        .join("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.country); })
-        .attr("width", x.bandwidth())
-        .attr("y", function(d) { return y(dropdown_count == "Raw"? d.weekly_count : d.normalized); })
-        .attr("height", function(d) { return height - y(dropdown_count == "Raw"? d.weekly_count : d.normalized); })
-        .style("fill", function (d) { return "rgb(0, 0, " +  100 + ")"; });
+  // append the rectangles for the bar chart
+  svg.selectAll(".bar")
+    .data(preProcessedData)
+    .join("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.country); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(dropdown_count == "Raw"? d.weekly_count : d.normalized); })
+      .attr("height", function(d) { return height - y(dropdown_count == "Raw"? d.weekly_count : d.normalized); })
+      .style("fill", function (d) { return "rgb(0, 0, " +  100 + ")"; });
 
-    // add the x Axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-            // .attr("text-anchor", "left")
-            .style("text-anchor", "end")
-            .style("font-size", "12px")
-            .attr("dx", "-.8em")
-            .attr("dy", "-.5em")
-            .attr("transform", "rotate(-90)");
-            // .attr("transform", "rotate(90 0 0)");
+  // add the x Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+        // .attr("text-anchor", "left")
+        .style("text-anchor", "end")
+        .style("font-size", "12px")
+        .attr("dx", "-.8em")
+        .attr("dy", "-.5em")
+        .attr("transform", "rotate(-90)");
+        // .attr("transform", "rotate(90 0 0)");
 
-    // add the y Axis
-    svg.append("g")
-        .call(d3.axisLeft(y));
+  // add the y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
-    });
-}
+});
 
 function preProcessCovidData(data) {
     
@@ -155,8 +119,8 @@ function preProcessCovidData(data) {
 
 function weekToString(week_nr) {
     startDayNr = 1 + (week_nr - 1) * 7;
-    startDate = new Date(2020, 0, startDayNr);
-    endDate = new Date(2020, 0, startDayNr + 6);
+    startDate = new Date(year, 0, startDayNr);
+    endDate = new Date(year, 0, startDayNr + 6);
     if (startDate.getFullYear() != endDate.getFullYear()) {
         endDate = new Date(startDate.getYear(), 
                             startDate.getMonth(),
@@ -164,43 +128,4 @@ function weekToString(week_nr) {
     }
     const dateFormat = { month: 'short', day: 'numeric' };
     return String(startDate.getDate()).padStart(2,"0") + "/" + String(startDate.getMonth() + 1).padStart(2,"0") + " - " + String(endDate.getDate()).padStart(2,"0") + "/" + String(endDate.getMonth() + 1).padStart(2,"0");
-}
-
-
-
-// DROPDOWN  - SET SELECTED FUNCTIONS
-
-function setSelectedContinent(dropdown) {
-    dropdown_continent = dropdown.options[dropdown.selectedIndex].value;
-    document.getElementsByTagName("svg")[0].innerHTML = "";
-    updateGraph();
-    console.log("set " + dropdown_continent);
-}
-
-function setSelectedIndicator(dropdown) {
-    dropdown_indicator = dropdown.options[dropdown.selectedIndex].value;
-    document.getElementsByTagName("svg")[0].innerHTML = "";
-    updateGraph();
-    console.log("set " + dropdown_indicator);
-}
-
-function setSelectedWeekNum(dropdown) {
-    dropdown_week = +dropdown.options[dropdown.selectedIndex].value;
-    document.getElementsByTagName("svg")[0].innerHTML = "";
-    updateGraph();
-    console.log("set " + dropdown_week);
-}
-
-function setSelectedYear(dropdown) {
-    dropdown_year = +dropdown.options[dropdown.selectedIndex].value;
-    document.getElementsByTagName("svg")[0].innerHTML = "";
-    updateGraph();
-    console.log("set " + dropdown_year);
-}
-
-function setSelectedCount(dropdown) {
-    dropdown_count = dropdown.options[dropdown.selectedIndex].value;
-    document.getElementsByTagName("svg")[0].innerHTML = "";
-    updateGraph();
-    console.log("set " + dropdown_count);
 }
